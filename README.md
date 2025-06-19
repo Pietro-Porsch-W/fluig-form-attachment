98640d121e5779da2ac55527cee545aea202aa72
Bruno Gasparetto <brunogasparetto@gmail.com>
2025-06-19 15:54:43 -0400

    Cria a base do plugin

---
+++ README.md
@@ -0,0 +1,141 @@
+# Fluig Form Attachment
+
+Plugin JQuery para auxiliar no tratamento de anexos em formulários
+de processo.
+
+A gestão dos anexos de processo no Fluig não é uma tarefa fácil. Além da aba anexos não
+permitir ordenação, o Fluig não permite, de maneira simples, vincular os anexos a campos
+do formulário do processo, assim devemos confiar que os usuários vão colocar os anexos
+corretamente na aba anexos e depois gastar tempo identificando cada anexo, pois usuários
+dificilmente respeitarão alguma regra de nomenclatura.
+
+Para permitir esse vínculo entre os campos do formulário e os anexos o
+[Sérgio Machado](https://www.linkedin.com/in/sergio-machado-analista-fluig/)
+criou o [ComponenteAnexos](https://github.com/sergiomachadosilva/fluig-utils/tree/main/projetos/ComponenteAnexos), que é uma biblioteca JS/CSS/HTML, permitindo um maior controle dos
+anexos. Esse projeto do Sérgio serviu como base para a construção desse Plugin JQuery.
+
+A intenção desse plugin é simplificar ainda mais o tratamento dos anexos em formulários
+que estão abertos em um Processo.
+
+## Atenção
+
+Esse plugin não funciona na versão mobile dentro do aplicativo My Fluig.
+
+## Instalação
+
+Basta adicionar o script `fluigFormAttachment.js` ou `fluigformAttachment.min.js` ao
+seu formulário.
+
+## Modo de usar
+
+Ao carregar o plugin no seu formulário ele ocultará a Aba Anexos do Fluig, evitando que
+insiram documentos não solicitados e, mais importante, removam anexos que já estão
+vinculados a um campo do formulário.
+
+### Básico
+
+Basta ter um campo do tipo texto e instanciar o plugin para esse campo.
+
+```html
+<div class="form-group">
+    <label for="cnh">Teste 001</label>
+    <input type="text" class="form-control" readonly name="cnh" id="cnh" data-filename="CNH">
+</div>
+```
+
+É recomendável deixar o campo como `readonly`. O plugin fará isso automaticamente quando
+instanciado no elemento, mas pode ser que algum usuário consiga editar o formulário fora
+do processo e nesse caso o plugin não funcionará.
+
+O atributo `data-filename` indica a "descrição" do anexo, que é o valor que aparece na Aba
+Anexos. Esse nome será utilizado para vincular o campo do formulário ao anexo.
+
+Então no JavaScript basta executar:
+
+```javascript
+$("#cnh").fluigFormAttachment();
+```
+
+### Parâmetros
+
+Ao instanciar o plugin para os elementos é possível passar um objeto com as seguintes
+propriedades de configuração:
+
+- **showActionButton**: indica se deve exibir os botões de Ação (Upload e Delete). `true` por padrão, `false` para não exibir (campo que não deve permitir upload/delete no momento);
+- **filename**: a descrição/nome do anexo. Caso exista o atributo `data-filename` no campo do formulário ele terá preferência;
+- **prefixName**:  Adiciona prefixo à descrição do anexo. `false` por padrão. `true` para prefixo aleatório (será usado um pedaço de um UUID) ou uma `string` para um prefixo fixo.
+
+Quando o campo estiver **Desabilitado** o botão de Upload/Delete não será exibido, mesmo se
+o parâmetro `showActionButton` estiver como `true`.
+
+A intenção da configuração `prefixName` é auxiliar nos casos de Tabela Pai Filho.
+
+Exemplos:
+
+```javascript
+$("#cnh").fluigFormAttachment({
+    showActionButton: false,
+});
+
+// Tabela Pai Filho
+$("#adicionar").on("click", function () {
+    const index = wdkAddChild("tabelaPaiFilho");
+    $(`#cnh___${index}`).fluigFormAttachment({
+        prefixName: true,
+    });
+});
+
+// Tabela Pai Filho com vários campos de anexo
+$("#adicionar").on("click", function () {
+    const index = wdkAddChild("tabelaPaiFilho");
+    const tr = $(`#cnh___${index}`).closest("tr");
+
+    // A cada upload será criado um prefixo aleatório pra cada anexo
+    $(".anexos", tr).fluigFormAttachment({
+        prefixName: true,
+    });
+});
+
+// Tabela Pai Filho com vários campos de anexo e prefixo único pra linha
+$("#adicionar").on("click", function () {
+    const index = wdkAddChild("tabelaPaiFilho");
+    const tr = $(`#cnh___${index}`).closest("tr");
+
+    // A cada upload usará o mesmo prefixo para todos os anexos da linha
+    $(".anexos", tr).fluigFormAttachment({
+        prefixName: FLUIGC.utilities.randomUUID().substring(0, 8),
+    });
+});
+```
+
+### Métodos
+
+É possível executar alguns métodos do Plugin para manipular os anexos. Os seguintes
+métodos estão disponíveis:
+
+- **hasAttachment**: Indica se o campo possuí anexo (tem descrição e o anexo está na tabela de anexos). Este método é executado em um único campo;
+- **isValid**: Indica se o campo está válido. Caso ele possua um valor (foi feito upload do anexo), mas o anexo não está na tabela de anexos, indicará campo inválido. Este método é executado em um único campo;
+- **deleteAttachment**: Remove o anexo daquele campo. Útil para quando excluir uma linha de uma tabela Pai Filho;
+
+Para executar um método basta chamar o plugin no campo indicando passando o nome do método ao invés
+do objeto de parâmetros.
+
+Exemplos:
+
+```javascript
+if ($("#cnh").fluigFormAttachment("hasAttachment")) {
+    // Exemplo: habilitar outro campo de preenchimento
+}
+
+function beforeSendValidate(numState, nextState) {
+    if (!$("#cnh").fluigFormAttachment("isValid")) {
+        throw "O Anexo da CNH não foi enviado corretamente. Remova-o e envie novamente";
+    }
+}
+
+// Removendo anexos da Pai Filho ao excluir linha
+$("#tabelaPaiFilho").on("click", ".removeItem", function() {
+    $(".anexos", this.closest("tr")).fluigFormAttachment("deleteAttachment");
+    fnWdkRemoveChild(this);
+});
+```
---
+++ fluigFormAttachment.js
@@ -0,0 +1,361 @@
+/**
+ * Plugin JQuery para trabalhar com anexos nos formulários dentro do processo
+ *
+ */
+
+
+/**
+ * Configurações
+ *
+ * @typedef AttachmentSettings
+ * @property {boolean} showActionButton Exibe o botão de upload/delete. True por padrão.
+ * @property {boolean} filename Nome que será salvo como descrição do Anexo. Preferencialmente usará o conteúdo do atributo data-filename do elemento.
+ * @property {boolean|string} prefixName Adiciona prefixo ao anexo. False por padrão, True para prefixo aleatório, String para prefixo fixo.
+ */
+
+;(function ($) {
+    "use strict";
+
+    const pluginName = "fluigFormAttachment";
+
+    const isString = item => typeof item === "string";
+
+    /**
+     * Configuração padrão
+     *
+     * @type {AttachmentSettings}
+     */
+    const defaults = {
+        showActionButton: true,
+        filename: "Anexo",
+        prefixName: false,
+    };
+
+    /**
+     * Métodos que serão executados somente no primeiro elemento
+     *
+     * @type {string[]}
+     */
+    const methodsOnlyFirstElement = [
+        "isValid",
+        "hasAttachment",
+    ];
+
+    class Plugin {
+        /**
+         * @type {AttachmentSettings}
+         */
+        #settings;
+
+        /**
+         * Elemento do arquivo. Pode ser um input ou span (no modo leitura).
+         *
+         * @type {JQuery<HTMLElement>}
+         */
+        #input;
+
+        /**
+         * @type {JQuery<HTMLElement>}
+         */
+        #container;
+
+        /**
+         * @type {string}
+         */
+        #attachmentFilename;
+
+        /**
+         * @param {HTMLElement} element
+         * @param {AttachmentSettings} options
+         */
+        constructor(element, options) {
+            this.#settings = $.extend({}, defaults, options);
+            this.#input = $(element);
+            this.#attachmentFilename = this.#input.val() || this.#input.text().trim();
+
+            this.#input
+                .prop("readonly", true)
+                .on("change", () => {
+                    this.#attachmentFilename = this.#input.val();
+                    this.#changeButtonsState();
+                })
+                .wrap(`<div class="${pluginName}Component"></div>`)
+                .after(`<div class="${pluginName}Component_buttons">${this.#getButtonsTemplate()}</div>`);
+
+            this.#container = this.#input.closest(`.${pluginName}Component`);
+
+            this.#container
+                .on("click", `.${pluginName}BtnDeleteFile`, () => this.#confirmDeleteAttachment())
+                .on("click", `.${pluginName}BtnUpuloadFile`, () => this.#uploadAttachment())
+                .on("click", `.${pluginName}BtnViewerFile`, () => this.#viewAttachment())
+            ;
+        }
+
+        /**
+         * Indica que o campo está válido
+         *
+         * Caso o campo possua algum valor é obrigatório que o anexo
+         * esteja na tabela de anexos.
+         *
+         * @returns {boolean}
+         */
+        isValid() {
+            return this.#attachmentFilename.length
+                ? this.hasAttachment()
+                : true
+            ;
+        }
+
+        /**
+         * Indica se o anexo está na tabela de anexos
+         *
+         * @returns {boolean}
+         */
+        hasAttachment() {
+            return this.#attachmentFilename.length > 0
+                && parent.ECM.attachmentTable.getData().findIndex(attachment => attachment.description === this.#attachmentFilename) !== -1;
+        }
+
+        /**
+         * Remove o anexo
+         *
+         * Método útil para excluir anexos em tabela Pai x Filho.
+         */
+        deleteAttachment() {
+            const attachmentIndex = parent.ECM.attachmentTable.getData().findIndex(attachment => attachment.description === this.#attachmentFilename);
+
+            setTimeout(() => this.#input.val("").trigger("change"), 500);
+
+            if (attachmentIndex === -1) {
+                return;
+            }
+
+            parent.WKFViewAttachment.removeAttach([attachmentIndex]);
+        }
+
+        #getButtonsTemplate() {
+            const hasFileSelected = this.#attachmentFilename.length !== 0;
+            const element = this.#input.get(0);
+
+            let buttonsTemplate = "";
+
+            if (this.#settings.showActionButton
+                && element.nodeName.toLowerCase() === "input"
+                && !element.disabled
+            ) {
+                buttonsTemplate += hasFileSelected
+                    ? `<button type="button" class="${pluginName}BtnAction ${pluginName}BtnDeleteFile btn btn-danger btn-sm" title="Remover Anexo"><i class="flaticon flaticon-trash icon-sm"></i></button>`
+                    : `<button type="button" class="${pluginName}BtnAction ${pluginName}BtnUpuloadFile btn btn-success btn-sm" title="Enviar Anexo"><i class="flaticon flaticon-upload icon-sm"></i></button>`
+                ;
+            }
+
+            buttonsTemplate += `<button type="button" class="${pluginName}BtnViewerFile btn btn-info btn-sm" ${hasFileSelected ? '' : 'style="display:none"'} title="Visualizar Anexo"><i class="flaticon flaticon-view icon-sm"></i></button>`;
+
+            return buttonsTemplate;
+        }
+
+        #changeButtonsState() {
+            const hasFileSelected = this.#attachmentFilename.length !== 0;
+
+            if (this.#settings.showActionButton) {
+                const buttonClasses = [];
+                let iconClass = "";
+                let title = "";
+
+                if (hasFileSelected) {
+                    buttonClasses.push(`${pluginName}BtnDeleteFile`);
+                    buttonClasses.push("btn-danger");
+                    iconClass = "flaticon-trash";
+                    title = "Remover Anexo";
+                } else {
+                    buttonClasses.push(`${pluginName}BtnUpuloadFile`);
+                    buttonClasses.push("btn-success");
+                    iconClass = "flaticon-upload";
+                    title = "Enviar Anexo";
+                }
+
+                this.#container.find(`.${pluginName}BtnAction`)
+                    .removeClass([`${pluginName}BtnUpuloadFile`, `${pluginName}BtnDeleteFile`, "btn-danger", "btn-success"])
+                    .addClass(buttonClasses)
+                    .attr("title", title)
+                    .trigger("blur")
+                    .find("i")
+                        .removeClass(["flaticon-trash", "flaticon-upload"])
+                        .addClass(iconClass)
+                ;
+            }
+
+            if (hasFileSelected) {
+                this.#container.find(`.${pluginName}BtnViewerFile`).show();
+            } else {
+                this.#container.find(`.${pluginName}BtnViewerFile`).hide();
+            }
+        }
+
+        #confirmDeleteAttachment() {
+            FLUIGC.message.confirm({
+                message: `Deseja remover o anexo <b>${this.#attachmentFilename}</b>?`,
+                title: 'Confirmação',
+                labelYes: 'Sim, quero remover',
+                labelNo: 'Não, quero cancelar',
+            }, result => {
+                if (!result) {
+                    return;
+                }
+
+                this.deleteAttachment();
+            });
+        }
+
+        #uploadAttachment() {
+            let filename = this.#input.data("filename") || this.#settings.filename;
+
+            if (this.#settings.prefixName === true) {
+                filename = FLUIGC.utilities.randomUUID().substring(0, 9) + filename;
+            } else if (this.#settings.prefixName !== false && isString(this.#settings.prefixName)) {
+                filename = `${this.#settings.prefixName}-${filename}`;
+            }
+
+            // Evitar conflito de descrição do anexo
+            if (parent.ECM.attachmentTable.getData().findIndex(attachment => attachment.description === filename) !== -1) {
+                FLUIGC.toast({
+                    title: "Atenção",
+                    message: "Já existe um anexo com esse nome",
+                    type: "warning",
+                })
+                return;
+            }
+
+            const uploadBtn = $("#ecm-navigation-inputFile-clone", parent.document)
+                .attr({
+                    "data-on-camera": "true",
+                    "data-file-name-camera": filename,
+                    "multiple": false,
+                })
+                .trigger("click");
+
+
+            uploadBtn.on(`change.${pluginName}`, (evt) => {
+                uploadBtn.off(`change.${pluginName}`);
+
+                const newAttachment = evt.target.files[0];
+
+                if (parent.ECM.maxUploadSize > 0 && newAttachment.size > parent.ECM.maxUploadSize * 1024 * 1024) {
+                    return;
+                }
+
+                // Na atividade início não permite arquivos duplicados, mas nas outras atividades é permitido
+                if (!parent.ECM.workflowView.processInstanceId) {
+                    const attachmentIndex = parent.ECM.attachmentTable.getData().findIndex(attachment => attachment.name === newAttachment.name);
+
+                    if (attachmentIndex !== -1) {
+                        return;
+                    }
+                }
+
+                setTimeout(() => this.#input.val(filename).trigger("change"), 500);
+            });
+        }
+
+        #viewAttachment() {
+            const attachmentIndex = parent.ECM.attachmentTable.getData().findIndex(attachment => attachment.description === this.#attachmentFilename);
+
+            if (attachmentIndex === -1) {
+                FLUIGC.toast({
+                    title: "Atenção",
+                    message: "Anexo não encontrado",
+                    type: "warning"
+                });
+
+                return;
+            }
+
+            const attachment = parent.ECM.attachmentTable.getRow(attachmentIndex);
+
+            if (attachment.documentId) {
+                parent.WKFViewAttachment.openAttachmentView(parent.WCMAPI.userCode, attachment.documentId, attachment.version);
+            } else {
+                parent.WKFViewAttachment.downloadAttach([attachmentIndex]);
+            }
+        }
+    }
+
+    /**
+     * Instancia o Plugin ou executa algum método do plugin
+     *
+     * @param {AttachmentSettings|string} options
+     * @returns {undefined|boolean|void}
+     */
+    $.fn[pluginName] = function (options) {
+        if (!parent.WKFViewAttachment || !parent.ECM || !parent.ECM.attachmentTable) {
+            console.error(`Plugin ${pluginName} executado fora de um processo.`)
+            return this;
+        }
+
+        // executa o método
+        if (isString(options)) {
+            const methodName = options;
+
+            if (methodsOnlyFirstElement.includes(methodName)) {
+                const element = $(this.get(0));
+                const data = element.data(pluginName);
+
+                if (!data || !data[methodName]) {
+                    return undefined;
+                }
+
+                return data[methodName]();
+            }
+
+            return this.each(function () {
+                const data = $(this).data(pluginName);
+
+                if (data && data[methodName]) {
+                    data[methodName]();
+                }
+            });
+        }
+
+        /**
+         * @type {AttachmentSettings}
+         */
+        const config = $.extend({}, defaults, options);
+
+        return this.each(function () {
+            if (!$.data(this, pluginName)) {
+                $.data(this, pluginName, new Plugin(this, config));
+            }
+        });
+    };
+
+    if (!parent.WKFViewAttachment || !parent.ECM || !parent.ECM.attachmentTable) {
+        return;
+    }
+
+    // Oculta aba de anexos
+    $(() => $("#tab-attachments", parent.document).hide());
+
+    $("head").append(`<style>
+.${pluginName}Component {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+}
+.${pluginName}Component input {
+    border-top-right-radius: 0 !important;
+    border-bottom-right-radius: 0 !important;
+}
+.${pluginName}Component_buttons {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+.${pluginName}Component_buttons .btn {
+    outline: none !important;
+    outline-offset: unset !important;
+    border-radius: 0 !important;
+    height: 32px;
+}
+</style>`);
+
+}(jQuery));
